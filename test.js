c8a4e7c9ce638f513a6f8d6700cf8104a2e9150e
nwrner <nwrner516@gmail.com>
2017-08-16 21:16:19 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 #JAVA TESTING GROUNDS
+
+#Iteration 1
 var user = new Object();
 user.name = "Gav";
 user.pass = "pass";
@@ -10,3 +12,16 @@ user.newPass = function(newPass) {
 
 user.newPass("pass2")
 console.log(user.pass)
+
+#iteration 2
+function entry(name, age, sex) {
+    this.name = name; 
+    this.age = age;
+    this.sex = sex;
+    this.getInfo = function() {
+        console.log("Name: " + this.name + " Age: " + this.age + " Sex: " + this.sex);
+    };
+}
+
+var nick = new entry("Michael", 22, "Male");
+nick.getInfo()
